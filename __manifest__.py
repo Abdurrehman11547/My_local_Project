5f002437210d8f47d17a9f7440402ac5b234089d
anasali650 <anas.indexword@gmail.com>
2025-05-27 20:11:14 +0500

    3rd yakki

--- __manifest__.py
+++ __manifest__.py
@@ -1,38 +1,28 @@
 # -*- coding: utf-8 -*-
 {
     'name': "School Management System",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
+    'summary': "School Management System for Odoo",
     'description': """
-Long description of module's purpose
+        Complete School Management System
+        ================================
+        Manage students, teachers, classes, and academic records.
     """,
-
     'author': "My Company",
     'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'category': 'Education',  # More specific category
+    'version': '17.0.1.0.0',  # Proper Odoo 17 versioning
     'depends': ['base'],
-
-    # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        # Only include files that actually exist
+        # 'views/templates.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        # 'demo/demo.xml',
     ],
-
     'installable': True,
     'application': True,
     'auto_install': False,
+    'license': 'LGPL-3',  # Add license
 }
-
